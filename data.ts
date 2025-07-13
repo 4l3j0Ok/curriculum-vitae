e114ebcd9226af8cac9709a9772733fd626847cf
4l3j0Ok <alejofsarmiento@gmail.com>
2025-07-13 04:05:44 -0300

    feat: migrate project to astro
    - Original project: https://github.com/4l3j0Ok/institute-second-year/tree/main/edi/01-curriculum-vitae

---
+++ data.ts
@@ -0,0 +1,158 @@
+export const experience = [
+  {
+    company: "Leafnoise",
+    position: "Desarrollador backend",
+    location: "Buenos Aires, Argentina",
+    period: "Agosto 2017 – Septiembre 2022",
+    responsibilities: [
+      "Desarrollo de producto Moorea para clientes de Leafnoise.",
+      "Creación de APIs y servicios web RESTful y SOAP.",
+      "Conexión de APIs con base de datos MongoDB y SQL.",
+      "Diseño y optimización de consulta para bases de datos.",
+      "Uso efectivo de Git para control de versiones.",
+      "Colaboración en equipos a través de plataformas como GitLab.",
+      "Metodologías ágiles y trabajo en equipo."
+    ]
+  },
+  {
+    company: "Leafnoise",
+    position: "SRE / DevOps",
+    location: "Buenos Aires, Argentina",
+    period: "Septiembre 2022 – Actualidad",
+    responsibilities: [
+      "Gestión y contenerización de aplicaciones web con Docker.",
+      "Automatización de despliegue con herramientas de CI/CD.",
+      "Orquestación de contenedores con Kubernetes.",
+      "Exposición de aplicaciones con servidores web Apache y NGINX.",
+      "Amplio conocimiento en servicios de Digital Ocean y bases de AWS con EC2, S3, ECS, entre otros.",
+      "Despliegue de servicios Java con Jboss EAP/ WildFly.",
+      "Conocimiento en herramientas de IaC como Terraform, Ansible, Vagrant.",
+      "Creación y administración de paquetes con Helm.",
+      "Monitoreo y observabilidad de aplicaciones.",
+      "Scripting con Bash, Powershell y Python."
+    ]
+  }
+];
+
+export const education = [
+  {
+    institution: "Escuela de Educación Secundaria Técnica N° 2 - Manuel Mateo",
+    degree: "Tecnicatura en informática con orientación en redes y programación",
+    location: "Buenos Aires, Argentina",
+    period: "Marzo 2012 – Noviembre 2019"
+  },
+  {
+    institution: "Instituto Superior de Formación Docente y Técnica Nº 93 Arturo Umberto Illia",
+    degree: "Tecnicatura Superior en Análisis, Desarrollo y Programación de Aplicaciones",
+    location: "Buenos Aires, Argentina",
+    period: "Marzo 2023 – Actualidad"
+  }
+];
+
+export const skills = {
+  "sections": [
+    {
+      "title": "Tecnologías que utilizo como desarrollador de aplicaciones:",
+      "categories": [
+        {
+          "name": "Lenguajes de programación",
+          "items": ["Python", "JavaScript", "TypeScript", "C#", "HTML", "CSS"]
+        },
+        {
+          "name": "Backend",
+          "items": ["Flask", "FastAPI"]
+        },
+        {
+          "name": "Frontend",
+          "items": ["Astro", "React"]
+        },
+        {
+          "name": "Base de datos",
+          "items": ["MongoDB", "SQL Server", "SQLite", "MySQL", "PostgreSQL", "Redis"]
+        },
+        {
+          "name": "Versionado",
+          "items": ["Git"]
+        }
+      ]
+    },
+    {
+      "title": "Tecnologías que utilizo como SRE:",
+      "categories": [
+        {
+          "name": "Infraestructura",
+          "items": ["Digital Ocean", "AWS"]
+        },
+        {
+          "name": "Contenedores",
+          "items": ["Docker", "Kubernetes", "Helm"]
+        },
+        {
+          "name": "CI/CD",
+          "items": ["Jenkins", "GitHub Actions", "GitLab CI", "ArgoCD"]
+        },
+        {
+          "name": "Servidores web",
+          "items": ["Apache", "Nginx"]
+        },
+        {
+          "name": "Monitorización",
+          "items": ["Grafana", "Prometheus", "Elastic Kibana"]
+        },
+        {
+          "name": "Scripting",
+          "items": ["Bash", "Python"]
+        },
+        {
+          "name": "IaC",
+          "items": ["Terraform"]
+        }
+      ]
+    }
+  ]
+}
+
+export const certifications = [
+  {
+    title: "Qt/PySide: Interfaces Gráficas con Python",
+    certificateUrl: "/alejo-sarmiento-certificado-pyside.pdf",
+    institution: "Hector Costa Guzmán",
+    date: "nov. 2024"
+  },
+  {
+    title: "Ultimate Git + GitHub",
+    certificateUrl: "/alejo-sarmiento-certificado-ultimate-git-github.pdf",
+    institution: "Academia Hola Mundo",
+    date: "ago. 2024"
+  },
+  {
+    title: "Ultimate Docker",
+    certificateUrl: "/alejo-sarmiento-certificado-ultimate-docker.pdf",
+    institution: "Academia Hola Mundo",
+    date: "jun. 2024"
+  },
+  {
+    title: "Ultimate Python",
+    certificateUrl: "/alejo-sarmiento-certificado-ultimate-python.pdf",
+    institution: "Academia Hola Mundo",
+    date: "abr. 2024"
+  },
+  {
+    title: "Python Sin Fronteras: HTML, CSS, Flask y MySQL",
+    certificateUrl: "/alejo-sarmiento-certificado-python-sin-fronteras.pdf",
+    institution: "Udemy",
+    date: "mar. 2024"
+  },
+  {
+    title: "Cloud Devops (Docker, Kubernetes y Jenkins)",
+    certificateUrl: "/alejo-sarmiento-certificado-cloud-devops.pdf",
+    institution: "Educación IT",
+    date: "may. 2022"
+  },
+  {
+    title: "Bases de datos NoSQL con MongoDB y NodeJS",
+    certificateUrl: "/alejo-sarmiento-certificado-mongodb.pdf",
+    institution: "Academia Mastermind",
+    date: "sept. 2022"
+  }
+];
